e12e6d41d4c60400d91932fbe64cef4143e07e5b
AlanYoung <2306915539@qq.com>
2021-03-08 22:14:50 +0800

    spring rabbitmq

---
+++ rabbitmq/src/main/java/com/yangxuan/spring/Foo.java
@@ -0,0 +1,8 @@
+package com.yangxuan.spring;
+
+public class Foo {
+
+    public void listen(String foo) {
+        System.out.println("消费者 " + foo);
+    }
+}
---
+++ rabbitmq/src/main/java/com/yangxuan/spring/SpringMain.java
@@ -0,0 +1,7 @@
+package com.yangxuan.spring;
+
+public class SpringMain {
+
+    public static void main(String[] args) {
+    }
+}
