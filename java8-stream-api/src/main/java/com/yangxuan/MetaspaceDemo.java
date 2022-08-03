bdcd0e5234f29bbc8918ebb46dc35daec833605b
杨选 <2306915539@qq.com>
2022-08-03 14:03:33 +0800

    OutOfMemoryError Metaspace

---
+++ java8-stream-api/src/main/java/com/yangxuan/MetaspaceDemo.java
@@ -0,0 +1,40 @@
+package com.yangxuan;
+
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+public class MetaspaceDemo {
+
+    static class OOM {
+    }
+
+    /**
+     * vm options -XX:MetaspaceSize=10m -XX:MaxMetaspaceSize=10m
+     *
+     * @param args .
+     */
+    public static void main(String[] args) {
+        int i = 0;//模拟计数多少次以后发生异常
+        try {
+            while (true) {
+                i++;
+                Enhancer enhancer = new Enhancer();
+                enhancer.setSuperclass(OOM.class);
+                enhancer.setUseCache(false);
+                enhancer.setCallback(new MethodInterceptor() {
+                    @Override
+                    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+                        return methodProxy.invokeSuper(o, args);
+                    }
+                });
+                enhancer.create();
+            }
+        } catch (Throwable e) {
+            System.out.println("=================多少次后发生异常：" + i);
+            e.printStackTrace();
+        }
+    }
+}
