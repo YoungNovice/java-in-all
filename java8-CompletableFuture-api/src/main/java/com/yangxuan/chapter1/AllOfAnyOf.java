a700d2e257dffdad339e4df824c2aa9e2dbf40b1
杨选 <2306915539@qq.com>
2022-08-08 10:58:06 +0800

    allof

---
+++ java8-CompletableFuture-api/src/main/java/com/yangxuan/chapter1/AllOfAnyOf.java
@@ -0,0 +1,54 @@
+package com.yangxuan.chapter1;
+
+import com.yangxuan.T;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * join()与get()区别在于join()返回计算的结果或者抛出一个unchecked异常(CompletionException)，而get()  返回一个具体的异常
+ * get()  可以指定超时时间
+ */
+public class AllOfAnyOf {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        m1();
+    }
+
+    public static void m1() throws ExecutionException, InterruptedException {
+        // 创建异步执行任务:
+        CompletableFuture<Double> cf = CompletableFuture.supplyAsync(() -> {
+            System.out.println(Thread.currentThread() + " start job1,time->" + System.currentTimeMillis());
+            T.sleep(2);
+            System.out.println(Thread.currentThread() + " exit job1,time->" + System.currentTimeMillis());
+            return 1.2;
+        });
+        CompletableFuture<Double> cf2 = CompletableFuture.supplyAsync(() -> {
+            System.out.println(Thread.currentThread() + " start job2,time->" + System.currentTimeMillis());
+            T.milli(1500);
+            System.out.println(Thread.currentThread() + " exit job2,time->" + System.currentTimeMillis());
+            return 3.2;
+        });
+        CompletableFuture<Double> cf3 = CompletableFuture.supplyAsync(() -> {
+            System.out.println(Thread.currentThread() + " start job3,time->" + System.currentTimeMillis());
+            T.milli(1300);
+//            throw new RuntimeException("test");
+            System.out.println(Thread.currentThread() + " exit job3,time->" + System.currentTimeMillis());
+            return 2.2;
+        });
+        //Allof等待所有任务执行完成才执行cf4，如果有一个任务异常终止，则cf4.get时会抛出异常，都是正常执行，cf4.get返回null
+        //anyOf是只有一个任务执行完成，无论是正常执行或者执行异常，都会执行cf4，cf4.get的结果就是已执行完成的任务的执行结果
+        CompletableFuture<Void> cf4 = CompletableFuture.allOf(cf, cf2, cf3).whenComplete((a, b) -> {
+            if (b != null) {
+                System.out.println("error stack trace->");
+                b.printStackTrace();
+            } else {
+                System.out.println("run success,result->" + a);
+            }
+        });
+        System.out.println("main thread start cf4.get(),time->" + System.currentTimeMillis());
+        //等待子任务执行完成
+        System.out.println("cf4 run result->" + cf4.get());
+        System.out.println("main thread exit,time->" + System.currentTimeMillis());
+    }
+}
